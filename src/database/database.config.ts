41e94a325e199c2bffbdb979827b4ad652e17b39
Gabriel Maximiniano <gabmaxsan@gmail.com>
2024-03-09 19:49:12 -0300

    add: mysql connect

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -2,9 +2,25 @@ import { OrderModel } from '../order/model/order.model';
 import { PaymentModel } from '../payment/model/payment.model';
 import { DataSourceOptions } from 'typeorm';
 
+const dbType = process.env.DB_TYPE || 'sqlite';
+
+const configs = {
+  sqlite: {
+    type: 'sqlite',
+    database: process.env.DB_DATABASE || '.db/sql',
+  },
+  mysql: {
+    type: 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_DATABASE || 'payment-ms',
+  }
+};
+
 export const config: DataSourceOptions = {
-  type: 'sqlite',
-  database: '.db/sql',
+  ...configs[dbType],
   synchronize: true,
   entities: [OrderModel, PaymentModel],
   // entities: [__dirname + '../**/model/*.model{.ts,.js}'],
